392911b8304b87f1d6f5f12cc08036c60753ed56
Duong Pham <bovame94@gmail.com>
2018-11-20 23:41:39 +0700

    part2

--- src/AutoCompleteContainer/index.jsx
+++ src/AutoCompleteContainer/index.jsx
@@ -1,46 +1,53 @@
 import React from 'react'
 import Autocomplete from 'react-autocomplete';
 import cities from './cities.json';
-
+import axios from 'axios';
 
 class AutoCompleteContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // Current value of the select field
       value: "",
-      // Data that will be rendered in the autocomplete
-      autocompleteData: [
-        {
-          label: 'Apple',
-          value: 1
-        },
-        {
-          label: 'Microsoft',
-          value: 2
-        },
-        {
-          label: 'Me, Myself and I',
-          value: 3
-        }
-      ]
+      autocompleteData: []
     };
   }
 
   onChange = (e) => {
     this.setState({
-      value: e.target.value
+      value: e.target.value,
+      autocompleteData: this.getSuggestions(e.target.value)
     });
-
-    console.log("The Input Text has changed to ", e.target.value);
   }
 
   onSelect = (val) => {
     this.setState({
       value: val
     });
+  }
 
-    console.log("Option from 'database' selected : ", val);
+  retrieveDataAsynchronously(searchText){
+    let url = ""
+    axios.get(url)
+    .then(response => {
+      this.setState({
+        autocompleteData: response.data
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    })
+  }
+
+  getSuggestions = (value) => {
+    const escapedValue = value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+    if (escapedValue === '') {
+      return [];
+    }
+
+    const regex = new RegExp('^' + escapedValue, 'i');
+
+    return cities.filter(city => regex.test(city.name));
   }
 
   renderItem(item, isHighlighted){
@@ -62,7 +69,7 @@ class AutoCompleteContainer extends React.Component {
       <div>
         <Autocomplete
           getItemValue={this.getItemValue}
-          items={cities}
+          items={this.state.autocompleteData}
           renderItem={this.renderItem}
           value={this.state.value}
           onChange={this.onChange}
